ce21f3c48734b2ccd0a91d89b7f399154ffc7eb6
Carlos Betancourt <carlos.betancourt@jalisco.gob.mx>
2021-06-24 12:04:51 -0500

    Add userdto to swagger

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,19 +1,24 @@
 import { IsNotEmpty, IsString, IsEmail } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateUserDto {
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   username: string;
 
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty()
   email: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   password: string;
 
   @IsNotEmpty()
   @IsString()
+  @ApiProperty()
   name: string;
 }
